cf0a2da0e94acd3fcd6451ccafeccc63d12707f3
Dev-X25874 <sayakmondal56@gmail.com>
2026-09-09 19:14:35 +0530

    Add Default impl for Metrics (clippy: new_without_default)

--- src/metrics.rs
+++ src/metrics.rs
@@ -33,6 +33,12 @@ pub struct Metrics {
     inner: Mutex<Inner>,
 }
 
+impl Default for Metrics {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Metrics {
     pub fn new() -> Self {
         Self { inner: Mutex::new(Inner::default()) }
